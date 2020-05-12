622e5b1fb5d34fc6d997ad88810fa8e3afe3893f
Vladyslav Avdiienko <44539631+vladyslav-avdiienko@users.noreply.github.com>
2020-05-12 23:57:57 +0300

    refactor: create builder in archive

--- src/main/java/ua/kpi/model/entity/Archive.java
+++ src/main/java/ua/kpi/model/entity/Archive.java
@@ -24,4 +24,65 @@ public class Archive {
 
     private LocalDate created;
     private LocalDate lastEdit;
+
+    private Archive(){
+
+    }
+
+    public Builder getBuilder() {
+        return new Builder();
+    }
+
+    public static Builder newBuilder() {
+        return new Archive().new Builder();
+    }
+
+    public class Builder {
+        private Builder() {}
+
+        public Archive build() {
+            return Archive.this;
+        }
+
+        public Builder setId(Long id) {
+            Archive.this.id = id;
+            return this;
+        }
+
+        public Builder setFullName(String fullName) {
+            Archive.this.fullName = fullName;
+            return this;
+        }
+
+        public Builder setWorkplace(String workplace) {
+            Archive.this.workplace = workplace;
+            return this;
+        }
+
+        public Builder setSalary(BigDecimal salary) {
+            Archive.this.salary = salary;
+            return this;
+        }
+
+        public Builder setCompanyName(String companyName) {
+            Archive.this.companyName = companyName;
+            return this;
+        }
+
+        public Builder setFinancialTurnover(BigDecimal financialTurnover) {
+            Archive.this.financialTurnover = financialTurnover;
+            return this;
+        }
+
+        public Archive.Builder setPersonType(PersonType personType) {
+            Archive.this.personType = personType;
+            return this;
+        }
+
+        public Archive.Builder setReportStatus(ReportStatus reportStatus) {
+            Archive.this.reportStatus = reportStatus;
+            return this;
+        }
+
+    }
 }
