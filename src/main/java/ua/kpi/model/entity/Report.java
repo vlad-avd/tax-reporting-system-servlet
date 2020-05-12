20a84da7e7baf5c21dbb9058209a34a76c10e029
Vladyslav Avdiienko <44539631+vladyslav-avdiienko@users.noreply.github.com>
2020-05-12 23:35:53 +0300

    refactor: add builder

--- src/main/java/ua/kpi/model/entity/Report.java
+++ src/main/java/ua/kpi/model/entity/Report.java
@@ -3,6 +3,7 @@ package ua.kpi.model.entity;
 import ua.kpi.model.enums.PersonType;
 import ua.kpi.model.enums.RejectionReason;
 import ua.kpi.model.enums.ReportStatus;
+import ua.kpi.model.enums.Role;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -24,4 +25,113 @@ public class Report {
 
     private LocalDate created;
     private LocalDate lastEdit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public BigDecimal getFinancialTurnover() {
+        return financialTurnover;
+    }
+
+    public PersonType getPersonType() {
+        return personType;
+    }
+
+    public ReportStatus getReportStatus() {
+        return reportStatus;
+    }
+
+    public RejectionReason getRejectionReason() {
+        return rejectionReason;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public LocalDate getLastEdit() {
+        return lastEdit;
+    }
+
+    private Report(){
+
+    }
+
+    public Builder getBuilder() {
+        return new Builder();
+    }
+
+    public static Builder newBuilder() {
+        return new Report().new Builder();
+    }
+
+    public class Builder {
+        private Builder() {}
+
+        public Report build() {
+            return Report.this;
+        }
+
+        public Builder setId(Long id) {
+            Report.this.id = id;
+            return this;
+        }
+
+        public Builder setFullName(String fullName) {
+            Report.this.fullName = fullName;
+            return this;
+        }
+
+        public Builder setWorkplace(String workplace) {
+            Report.this.workplace = workplace;
+            return this;
+        }
+
+        public Builder setSalary(BigDecimal salary) {
+            Report.this.salary = salary;
+            return this;
+        }
+
+        public Builder setCompanyName(String companyName) {
+            Report.this.companyName = companyName;
+            return this;
+        }
+
+        public Builder setFinancialTurnover(BigDecimal financialTurnover) {
+            Report.this.financialTurnover = financialTurnover;
+            return this;
+        }
+
+        public Builder setPersonType(PersonType personType) {
+            Report.this.personType = personType;
+            return this;
+        }
+
+        public Builder setReportStatus(ReportStatus reportStatus) {
+            Report.this.reportStatus = reportStatus;
+            return this;
+        }
+
+    }
 }
