18e623135bad2197d8708028f877a7b11a90d524
Vladyslav Avdiienko <avdienko.vladyslav@gmail.com>
2020-05-19 19:13:48 +0300

    rename file

--- src/main/java/ua/kpi/controller/action/user/ReportAction.java
+++ src/main/java/ua/kpi/controller/action/user/ReportAction.java
@@ -3,14 +3,14 @@ package ua.kpi.controller.action.user;
 import ua.kpi.controller.action.Action;
 import ua.kpi.service.ReportService;
 import ua.kpi.service.ReportServiceImpl;
-
+import ua.kpi.model.entity.Report;
 import javax.servlet.http.HttpServletRequest;
 
 import java.util.List;
 
 import static ua.kpi.constant.Pages.*;
 
-public class Report implements Action {
+public class ReportAction implements Action {
 
     ReportService reportService = new ReportServiceImpl();
 
@@ -19,11 +19,11 @@ public class Report implements Action {
         String reportId = request.getParameter("id");
         if(reportId != null){
             Long id = Long.parseLong(reportId);
-            ua.kpi.model.entity.Report report = reportService.getReportById(id);
+            Report report = reportService.getReportById(id);
             request.setAttribute("report", report);
             return ROOT_FOLDER + USER_PAGES + REPORT;
         }
-        List<ua.kpi.model.entity.Report> reports;
+        List<Report> reports;
         Long user_id = Long.parseLong(request.getSession().getAttribute("userId").toString());
         reports = reportService.getReportsByUserId(user_id);
         request.setAttribute("reports", reports);
