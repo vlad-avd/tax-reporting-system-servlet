88ab5ca8353809aaa31fb4e7e6c108bff1cd77d9
Vladyslav Avdiienko <avdienko.vladyslav@gmail.com>
2020-06-03 17:37:23 +0300

    feat: create request parameters setter

---
+++ src/main/java/ua/kpi/util/RequestParametersSetter.java
@@ -0,0 +1,22 @@
+package ua.kpi.util;
+
+import java.util.List;
+import java.util.Map;
+
+public class RequestParametersSetter {
+    public String setParameters(Map<String, String> parameters) {
+        StringBuilder requestParameters = new StringBuilder("?");
+
+        for(Map.Entry<String, String> parameter : parameters.entrySet()) {
+            requestParameters
+                    .append("&")
+                    .append(parameter.getKey())
+                    .append("=")
+                    .append(parameter.getValue());
+        }
+
+        requestParameters.deleteCharAt(1); // delete first &
+
+        return requestParameters.toString();
+    }
+}
